a8bdc90b2fe7c38dfe668417d00f9d94605feeb3
lazycodex <Hope_Of_dfinal@hotmail.com>
2021-07-25 03:39:01 +0700

    fix some bugs when cropping

--- frontend/app/public/assets/js/home.js
+++ frontend/app/public/assets/js/home.js
@@ -46,7 +46,7 @@
       $modal.on('shown.bs.modal', function () {
         cropper = new Cropper(image, {
           aspectRatio: 1,
-          viewMode: 3,
+          viewMode: 1,
         });
       }).on('hidden.bs.modal', function () {
         cropper.destroy();
