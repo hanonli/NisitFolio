4375ce20405a587872ac8f38271178ad6b4a6191
pant411 <pantchanit2561@hotmail.com>
2021-07-26 09:31:32 +0700

    create landingPage1-4

---
+++ frontend/app/src/Components/landingPage1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class landingPage1 extends React.Component {
+    render() {
+        return (
+            <div className="landingPage1">
+
+            </div>
+        );
+    }
+}
+
+export default landingPage1;
---
+++ frontend/app/src/Components/landingPage2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class landingPage2 extends React.Component {
+    render() {
+        return (
+            <div className="landingPage2">
+
+            </div>
+        );
+    }
+}
+
+export default landingPage2;
