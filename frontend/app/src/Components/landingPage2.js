af971dcc63deac55625356a1d9fb45de1e71b454
phattharapolb <phattharapolb@gmail.com>
2021-07-26 21:05:36 +0700

    updated landingpage

--- frontend/app/src/Components/landingPage2.js
+++ frontend/app/src/Components/landingPage2.js
@@ -6,23 +6,18 @@ class LandingPage2 extends React.Component {
         return (
             <div className="landingPage">
                 <span class="lg-view-search mx-auto">
-                <div className="container-fluid">
+                    <div className="container-fluid">
                         <div class="d-flex justify-content-center">
                             <div className="row justify-content-md-center">
-                                <div className="landingpage2">
-                                    <div className="col-md-auto">
-                                        <img src="assets/images/MyResume.png" className="LRounded" />
-                                    </div>
-                                    <div className="Myresume-text" >
-                                        <h1>MyResume</h1> <br/>
-                                        <h3>สร้างเรซูเม่ด้วยเทมเพลตที่ใช้ง่าย</h3>
-                                    </div>
+                                <div className="col-md-auto">
+                                    <img src="assets/images/MyResume.png" className="LRounded" />
                                 </div>
                                 <div className="col-md-auto">
                                     <img src="assets/images/Portfolio.png" className="LRounded" />
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </span>
 
