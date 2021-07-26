40aee24349ff5907fed7cabf835d7e908416ea61
lazycodex <Hope_Of_dfinal@hotmail.com>
2021-07-26 23:42:50 +0700

    fix bugs

--- frontend/app/src/Components/profileContent.js
+++ frontend/app/src/Components/profileContent.js
@@ -12,16 +12,16 @@ class ProfileContent extends React.Component {
 											<div class="col-md-auto">
 												<Link to="/myresume">
 													<div class="transition-component scale-up-s resume-icon" id="cross-fade">
-														<img class="resume-icon bottom" src="assets/images/myresume2.png" type='button' id="myresume" alt="" />
-														<img class="resume-icon top" src="assets/images/myresume1.png" type='button' id="myresume" alt="" />
+														<img class="resume-icon bottom" src="assets/images/myresume2.png" type='button' id="myresume-home" alt="" />
+														<img class="resume-icon top" src="assets/images/myresume1.png" type='button' id="myresume-home" alt="" />
 													</div>
 												</Link>
 											</div>
 											<div class="col-md-auto">
 												<Link to="/portfolio">
 													<div class="transition-component scale-up-s portfolio-icon" id="cross-fade">
-														<img class="portfolio-icon bottom" src="assets/images/portfolio2.png" type='button' id="portfolio" alt="" />
-														<img class="portfolio-icon top" src="assets/images/portfolio1.png" type='button' id="portfolio" alt="" />
+														<img class="portfolio-icon bottom" src="assets/images/portfolio2.png" type='button' id="portfolio-home" alt="" />
+														<img class="portfolio-icon top" src="assets/images/portfolio1.png" type='button' id="portfolio-home" alt="" />
 													</div>
 												</Link>
 											</div>
@@ -29,16 +29,16 @@ class ProfileContent extends React.Component {
 												<div class="col-md-auto profile-sm">
 													<Link to="/analytic">
 														<div class="transition-component scale-up-s analytic-icon" id="cross-fade">
-															<img class="analytic-icon bottom" src="assets/images/analytics2.png" type='button' id="analytic" alt="" />
-															<img class="analytic-icon top" src="assets/images/analytics1.png" type='button' id="analytic" alt="" />
+															<img class="analytic-icon bottom" src="assets/images/analytics2.png" type='button' id="analytic-home" alt="" />
+															<img class="analytic-icon top" src="assets/images/analytics1.png" type='button' id="analytic-home" alt="" />
 														</div>
 													</Link>
 												</div>
 												<div class="col-md-auto">
 													<Link to="/bookmark">
 														<div class="transition-component scale-up-s bookmark-icon" id="cross-fade">
-															<img class="bookmark-icon bottom" src="assets/images/bookmark2.png" type='button' id="bookmark" alt="" />
-															<img class="bookmark-icon top" src="assets/images/bookmark1.png" type='button' id="bookmark" alt="" />
+															<img class="bookmark-icon bottom" src="assets/images/bookmark2.png" type='button' id="bookmark-home" alt="" />
+															<img class="bookmark-icon top" src="assets/images/bookmark1.png" type='button' id="bookmark-home" alt="" />
 														</div>
 													</Link>
 												</div>
