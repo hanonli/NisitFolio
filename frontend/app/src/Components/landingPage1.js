b166b2fa7516685d29dfa473ef14693e1aa70074
pant411 <pantchanit2561@hotmail.com>
2021-07-28 00:08:43 +0700

    add search bar na hee

--- frontend/app/src/Components/landingPage1.js
+++ frontend/app/src/Components/landingPage1.js
@@ -13,6 +13,14 @@ class LandingPage1 extends React.Component {
                 <div className="textOfLandingPage">
                     <h1 id="sloganNisitZaa">เริ่มต้นชีวิตในรั้วมหาลัยที่ดีกว่า<br />ด้วยระบบจัดเก็บและโชว์ผลงานที่ดีกว่า</h1>
                 </div>
+                <div class="lg-view-search container-fluid container-search-no-icons" id="fromNahee">
+                    <form class="d-flex" >
+                        <input class="form-control form-control-lg btn-search-box" type="search" placeholder="ค้นหาบุคคลและผลงานคุณภาพได้ที่นี่" aria-label="Search" />
+                        <button class="btn btn-search yellow" type="submit">
+                            <img src="assets/images/search.png" alt="" width="65" height="65" />
+                        </button>
+                    </form>
+                </div>
             </div>
         );
     }
