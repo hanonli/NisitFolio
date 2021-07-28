b9bbb9a3764ff96d7524ef0505ee32989a0705c7
phattharapolb <phattharapolb@gmail.com>
2021-07-29 04:33:41 +0700

    fixed text and arrow

--- frontend/app/src/Components/landingPage1.js
+++ frontend/app/src/Components/landingPage1.js
@@ -16,7 +16,7 @@ class LandingPage1 extends React.Component {
                     <form class="d-flex" >
                         <input class="form-control form-control-lg btn-search-box" id="stupidSearch" type="search" placeholder="ค้นหาบุคคลและผลงานคุณภาพได้ที่นี่" aria-label="Search" />
                         <button class="btn btn-lg btn-search yellow" type="submit" >
-                            <img src="assets/images/search.png" alt="" width="65" height="65" />
+                            <img src="assets/images/search.png" alt="" width="40" height="40" />
                         </button>
                     </form>
                 </div>
