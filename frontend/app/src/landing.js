8befb79c2e0a7d198dca24ec06bc8907ddf4792a
phattharapolb <phattharapolb@gmail.com>
2021-07-29 03:40:09 +0700

    landingpage1 change from absolute to relative

--- frontend/app/src/landing.js
+++ frontend/app/src/landing.js
@@ -4,6 +4,7 @@ import './index.css';
 import NavbarTransparent from './Components/navbarTransparent';
 import LandingHeader from './Components/landingHeader';
 import LandingBody from './Components/landingBody';
+import LandingPage1 from './Components/landingPage1';
 import LandingPage2 from './Components/landingPage2';
 import LandingPage3 from './Components/landingPage3';
 import reportWebVitals from './reportWebVitals';
@@ -25,8 +26,7 @@ class Landing extends React.Component {
 						<a class="btn btn-cta-primary-yellow round" href="#" target="_blank">สมัครสมาชิก</a>
 					</Link>
 				</div>
-				<LandingHeader />
-				<LandingBody />
+				<LandingPage1/>
 				<LandingPage2/>
 				<LandingPage3/>
 			</div>
