0e70f610c4ba05aeaa748414b47e1cc8764f3173
phattharapolb <phattharapolb@gmail.com>
2021-07-29 02:00:02 +0700

    update use <viewport>

--- frontend/app/src/Components/landingPage2.js
+++ frontend/app/src/Components/landingPage2.js
@@ -5,8 +5,9 @@ class LandingPage2 extends React.Component {
     render() {
         return (
             <div className="landingPage2">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
                 <div className="container-fluid text-center">
-                    <div class="d-flex justify-content-center" id="shownav">
+                    <div class="d-flex justify-content-center">
                         <div className="row">
                             <div className="col-lg-12" id="div1">
                                 <img src="assets/images/MyResume.png" className="LRounded1" />
@@ -17,6 +18,7 @@ class LandingPage2 extends React.Component {
                                 <img src="assets/images/Portfolio.png" className="LRounded2" />
                                 <h1 id="portfolio">Portfolio</h1>
                                 <h1 id="textMyPortfolio">แหล่งจัดเก็บผลงานของคุณตั้งแต่เริ่มใช้งานจนถึงปัจจุบัน</h1>
+                                
                             </div>
                         </div>
                     </div>
