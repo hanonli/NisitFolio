8c30144ff7c41b0f9078f1cca258d3fa45b20f48
pant411 <pantchanit2561@hotmail.com>
2021-07-30 02:23:24 +0700

    stupid landingpage1 almost complete

--- frontend/app/src/Components/landingPage1.js
+++ frontend/app/src/Components/landingPage1.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import './landingPage1.css';
-import SignInLand from './signIn_land'
+import SignInLand from './signIn_land';
+import SearchLanding1 from './searchLanding'
 
 class LandingPage1 extends React.Component {
     render() {
         return (
-            <div className="landingPage1">
-                <header>    
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-                </header>                
-                <div className="LogoNisitZaa11244">
-                    <img src="assets/images/nisitfolio_logo_white.png" width="665" id="nisit44Zaa112" />
-                </div>
-                <div className="textOfLandingPage">
-                    <h1 id="sloganNisitZaa">เริ่มต้นชีวิตในรั้วมหาลัยที่ดีกว่า<br />ด้วยระบบจัดเก็บและโชว์ผลงานที่ดีกว่า</h1>
-                </div>
-                <div class="lg-view-search container-fluid container-search-no-icons" id="fromNahee">
-                    <form class="d-flex" >
-                        <input class="form-control form-control-lg btn-search-box" id="stupidSearch" type="search" placeholder="ค้นหาบุคคลและผลงานคุณภาพได้ที่นี่" aria-label="Search" />
-                        <button class="btn btn-lg btn-search yellow" type="submit" >
-                            <img src="assets/images/search.png" alt="" width="40" height="40" />
-                        </button>
-                    </form>
-                </div>
-                <div className="signInX" >
-                    <SignInLand />
-                </div>
-                <img src="assets/images/arrow_down1.png" width="110" id="arrow_down1" />
-                <h1 id="textOfArrow">ฟีเจอร์ที่เรามีไว้ช่วยคุณ</h1>
+            <div className="landingPage1">      
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12" >
+                            <img src="assets/images/nisitfolio_logo_white.png" id="nisit44Zaa112" />
+                            <div className="textOfLandingPage">
+                                <h1 id="sloganNisitZaa">เริ่มต้นชีวิตในรั้วมหาลัยที่ดีกว่า<br />ด้วยระบบจัดเก็บและโชว์ผลงานที่ดีกว่า</h1>
+                            </div>                              
+                        </div>                        
+                    </div>
+                    <div class="row" id="searchHuaKuay">
+                        <div class="col-sm-6" >
+                            <SearchLanding1  />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div className="signInX" >
+                                <SignInLand />
+                            </div>                             
+                        </div>                        
+                    </div>
+                </div>   
+                <div class="row">
+                        <div class="col">
+                            <img src="assets/images/arrow_down1.png" width="110" id="arrow_down1" />
+                            <h1 id="textOfArrow">ฟีเจอร์ที่เรามีไว้ช่วยคุณ</h1>     
+                        </div>                           
+            
+                </div>          
+ 
             </div>
         );
     }
