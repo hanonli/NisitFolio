753da346c37d9d7c12a779ca20da5933fe0e8d64
phattharapolb <phattharapolb@gmail.com>
2021-07-31 05:53:06 +0700

    fix layout

--- frontend/app/src/Components/landingPage4.js
+++ frontend/app/src/Components/landingPage4.js
@@ -18,7 +18,7 @@ class LandingPage4 extends React.Component {
                                     <h1 id='textsharing'>แชร์เรซูเม่อันทรงพลังของคุณให้กับเพื่อนๆหรือคนที่สนใจได้อย่างง่ายดายด้วยระบบ QR Code และ PDF ที่เป็นที่นิยม</h1>   
                                     <h1 id='textinvite'>เริ่มต้น<br/>ชีวิตที่ดีกว่า<br/>เพียงแค่คลิก</h1> 
                                     <Link to="/agreement">
-                                        <button className="btn default" id="buttonRegisLanding" ><h1 id='buttontextRegis'>สมัครสมาชิก</h1></button>
+                                        <button className="btn" id="buttonRegisLanding" ><h1 id='buttontextRegis'>สมัครสมาชิก</h1></button>
 						            </Link>
                                 </div> 
                             
