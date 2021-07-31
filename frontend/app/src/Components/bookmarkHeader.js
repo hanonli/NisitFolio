dffb01b05ee87af086024e4a48ca513f5d659c6f
lazycodex <Hope_Of_dfinal@hotmail.com>
2021-07-31 20:05:43 +0700

    add bookmark header

--- frontend/app/src/Components/bookmarkHeader.js
+++ frontend/app/src/Components/bookmarkHeader.js
@@ -5,7 +5,20 @@ class BookmarkHeader extends React.Component {
 	render (){
 		return (
 			<div className="BookmarkHeader">
-				
+				<header class="header-white">
+					<div class="container-fluid">     
+						<div class="row">
+							<div class="col">
+								<div class="topDataBk-content">
+									<h1 class="name inline">บุ๊คมาร์ค</h1>
+									<h1 class="symbol inline">.</h1>
+									<h1 class="name2 inline">"วรเมศ"</h1>
+									<h1></h1>
+								</div>
+							</div>
+						</div>        
+					</div>
+				</header>
 			</div>
 		);
 	}
