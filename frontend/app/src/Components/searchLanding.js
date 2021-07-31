23d7692337fd995717578d5c04bb9df2e9b4aa8f
pant411 <pantchanit2561@hotmail.com>
2021-08-01 00:20:19 +0700

    update landingpage1

--- frontend/app/src/Components/searchLanding.js
+++ frontend/app/src/Components/searchLanding.js
@@ -3,12 +3,16 @@ import React from 'react';
 class SearchLanding1 extends React.Component {
   render() {
     return (
-      <form class="d-flex" >
-        <input class="form-control btn-search-box home" id="stupidSearch" type="search" placeholder="ค้นหาบุคคลและผลงานคุณภาพได้ที่นี่" aria-label="Search" />
-        <button class="btn btn-search yellow" type="submit" >
-          <img src="assets/images/search.png" alt="" id="btnstupidSearch" />
-        </button>
-      </form>
+      <div className="app_search">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <form class="d-flex" >
+          <input class="form-control btn-search-box home" id="stupidSearch" type="search" placeholder="ค้นหาบุคคลและผลงานคุณภาพได้ที่นี่" aria-label="Search" />
+          <button class="btn btn-search yellow" type="submit" id="width_button" >
+            <img src="assets/images/search.png" alt="" id="btnstupidSearch" />
+          </button>
+        </form>        
+      </div>
+
 
     );
   }
