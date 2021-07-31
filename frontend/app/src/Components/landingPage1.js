5d38a1d25769eda615531cddd0409e89d14c32c9
lazycodex <Hope_Of_dfinal@hotmail.com>
2021-08-01 05:36:12 +0700

    update landingpage1

--- frontend/app/src/Components/landingPage1.js
+++ frontend/app/src/Components/landingPage1.js
@@ -1,35 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './landingPage1.css';
+import './landingPage4.css'
 import SignInLand from './signIn_land';
 import SearchLanding1 from './searchLanding'
 
-class LandingPage1 extends React.Component {
+class LandingPage4 extends React.Component {
     render() {
         return (
-            <div className="landingPage1">
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-                <img src="assets/images/ldwithgradient.png" id="bg_landingPage44" />
-                <div class="container-fliud">
-                    <div class="row">
-                        <img src="assets/images/nisitfolio_logo_white3.png" id="nisit44Zaa112" />
-                        <div className="textOfLandingPage">
-                            <h1 id="sloganNisitZaa">เริ่มต้นชีวิตในรั้วมหาลัยที่ดีกว่า<br />ด้วยระบบจัดเก็บและโชว์ผลงานที่ดีกว่า</h1>
-                        </div>
-                        <div class="col-sm-6" id="searchHuaKuay">
-                            <SearchLanding1 />
-                        </div>       
-                        <div id="signInX">
-                            <SignInLand />
-                        </div>                                                 
-
-
-
-
-
-
-                        <div class="col">
-                            <img src="assets/images/arrow_down1.png" width="110" id="arrow_down1" />
-                            <h1 id="textOfArrow">ฟีเจอร์ที่เรามีไว้ช่วยคุณ</h1>
+            <div className="landingPage4">
+                <header>    
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                </header>
+                <div className="container">
+                    <div class="d-flex justify-content-center">
+                        <div>
+                                    <img class="pic-new" src="assets/images/ldwithgradient.png"/>
+									<img class="pic-logo" src="assets/images/nisitfolio_logo_white3.png"/>
+                                    <h1 class ="sc1-sub">เริ่มต้นชีวิตในรั้วมหาลัยที่ดีกว่า<br />ด้วยระบบจัดเก็บและโชว์ผลงานที่ดีกว่า</h1>   
+									<div class="col-6" id="searchHuaKuay">
+										<SearchLanding1 />
+									</div> 
+                                   
+									<div id="signInX">
+										<SignInLand />
+									</div>   
+									<img class="sc1-footer" src="assets/images/arrow_down1.png"/>
+									<h1 class="text-of-arrow">ฟีเจอร์ที่เรามีไว้ช่วยคุณ</h1>
                         </div>
                     </div>
                 </div>
@@ -38,4 +35,4 @@ class LandingPage1 extends React.Component {
     }
 }
 
-export default LandingPage1;
+export default LandingPage4;
