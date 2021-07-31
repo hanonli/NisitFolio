9199035ec175cf11e8ab70de1bf0ce32444879fa
pant411 <pantchanit2561@hotmail.com>
2021-07-31 22:27:06 +0700

    update landingPage1

--- frontend/app/src/Components/searchLanding.js
+++ frontend/app/src/Components/searchLanding.js
@@ -4,8 +4,8 @@ class SearchLanding1 extends React.Component {
   render() {
     return (
       <form class="d-flex" >
-        <input class="form-control form-control-lg btn-search-box" id="stupidSearch" type="search" placeholder="ค้นหาบุคคลและผลงานคุณภาพได้ที่นี่" aria-label="Search" />
-        <button class="btn btn-lg btn-search yellow" type="submit" >
+        <input class="form-control btn-search-box home" id="stupidSearch" type="search" placeholder="ค้นหาบุคคลและผลงานคุณภาพได้ที่นี่" aria-label="Search" />
+        <button class="btn btn-search yellow" type="submit" >
           <img src="assets/images/search.png" alt="" id="btnstupidSearch" />
         </button>
       </form>
