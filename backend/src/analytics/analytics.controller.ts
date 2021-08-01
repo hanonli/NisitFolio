85ddaf16b5797c60705ed2bc7d13118591b96987
Peem <intamoon.peem@gmail.com>
2021-08-01 19:57:39 +0700

    Add query additionalskill + add connection to cloud

--- backend/src/analytics/analytics.controller.ts
+++ backend/src/analytics/analytics.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from '@nestjs/common';
+import { AdditionalSkill } from './analytics.schema';
+import { AnalyticsService } from './analytics.service';
 
 @Controller('analytics')
 export class AnalyticsController {
-  
+  constructor(private analyticsService: AnalyticsService) {}
+
+  @Get('/additional')
+  async findAddSkill(): Promise<AdditionalSkill[]> {
+    return this.analyticsService.findAddSkill();
+  }
 }
