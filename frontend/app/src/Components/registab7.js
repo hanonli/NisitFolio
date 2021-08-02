ac159e2312c2d1e596da0c85736d08801b13a6a9
rungrung64 <rungrung64@hotmail.com>
2021-08-03 05:43:41 +0700

    test

--- frontend/app/src/Components/registab7.js
+++ frontend/app/src/Components/registab7.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './register.css';
 
 class Registab7 extends React.Component {
 	render (){
 		return (
 			<div className="Registab7">
-				<div>
+				<div class="regis-box-content">
 					<h1>"This is Tab7..."</h1>
 				</div>
 			</div>
