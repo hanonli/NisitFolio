d1c80cb977f0e2a8103bbad9f76aff256fd49334
pant411 <pantchanit2561@hotmail.com>
2021-08-03 22:02:56 +0700

    add select year

--- frontend/app/src/Components/popUpJobInterest.js
+++ frontend/app/src/Components/popUpJobInterest.js
@@ -4,7 +4,7 @@ import './popUpJobInterest.css';
 class PopUp_Job_interest extends React.Component {
     render() {
         return (
-            <div className="Popup_jop">
+            <div className="popup_jop">
                 <div className='popup_inner_job'>
                     <h1>{this.props.text}</h1>
                     <button onClick={this.props.closePopup}>close me</button>
--- frontend/app/src/Components/popUp_certificate.js
+++ frontend/app/src/Components/popUp_certificate.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './popUp_certificate.css';
 
 class PopUp_certi extends React.Component {
+    componentDidMount() {
+        const script = document.createElement("script");
+        script.src = "assets/js/date.js";
+        document.body.appendChild(script);
+    }
+
     render() {
         return (
-            <div className="Popup_certi">
+            <div className="popup_certi">
                 <div className='popup_inner_certi'>
-                    <h1>{this.props.text}</h1>
+                    <form class="name_certi">
+                        <input type="text" id="certi_name" name="certi_name" placeholder="พิมพ์ชื่อใบรับรอง/เกียรติบัตร"></input>
+                    </form>
+                    <form class="year_certi">
+                        <select name="yearpicker" id="yearpicker"></select>
+                    </form>
                     <button onClick={this.props.closePopup}>close me</button>
                 </div>
             </div>
