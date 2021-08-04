b1766a3d529e43039228cfa33003749069666e39
phattharapolb <phattharapolb@gmail.com>
2021-08-04 21:09:50 +0700

    update 4/8/2564

--- frontend/app/src/Components/registab3.js
+++ frontend/app/src/Components/registab3.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './registertab3.css'
 
 class Registab3 extends React.Component {
 	render (){
 		return (
 			<div className="Registab3">
-				<h1>"This is Tab3..."</h1>
+				<div className='textbox-container justify-content-center'>
+					
+						<div className='textbox'>
+							<h1>อุดมศึกษา</h1>
+
+							<button type="button" class="btn item-align-center" >
+								<img id='icon-plus-circle'  src="assets/images/+.png"></img>
+							</button>
+						</div>
+
+						<div className='textbox'>
+							<h1>มัธยมศึกษา</h1>
+							<button type="button" class="btn item-align-center" >
+								<img id='icon-plus-circle'  src="assets/images/+.png"></img>
+							</button>
+						</div>
+
+				</div>
 			</div>
 		);
 	}
