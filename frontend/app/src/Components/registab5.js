8659d48b1c007d209cf2799868c70826544d4282
pant411 <pantchanit2561@hotmail.com>
2021-08-04 11:05:26 +0700

    remove popup

--- frontend/app/src/Components/registab5.js
+++ frontend/app/src/Components/registab5.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import './register.css';
-import PopUp_certi from './popUp_certificate'
 
 class Registab5 extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			showPopup: false
-		};
-	}
-	togglePopup() {
-		this.setState({
-			showPopup: !this.state.showPopup
-		});
-	}
 	render() {
 		return (
 			<div className="Registab5">
 				<div class="regis-box-content">
 					<h1>hihi</h1>
-					<button onClick={this.togglePopup.bind(this)}>show popup</button>
-					{this.state.showPopup ?
-						<PopUp_certi text='Close Me' closePopup={this.togglePopup.bind(this)} />
-						: null
-					}
 				</div>
 			</div>
 		);
