d0fb368f07eb46d6aee97c4d8d90a6481822f7d2
pant411 <pantchanit2561@hotmail.com>
2021-08-04 22:07:37 +0700

    add popupJOB

--- frontend/app/src/Components/registab6.js
+++ frontend/app/src/Components/registab6.js
@@ -23,11 +23,7 @@ class Registab6 extends React.Component {
 					<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 						<div class="modal-dialog modal-dialog-centered">
 							<div class="modal-content minisize">
-								<h4 class="del-b">คุณต้องการลบทักษะเสริม ?</h4>
-								<div class="centerverify">
-									<a type="button" class="btn btn-cta-primary-svshort round profile-button grey margin-right-m" data-bs-dismiss="modal">ยกเลิก</a>
-									<a type="button" class="btn btn-cta-primary-yellowshort profile-button round">ลบ</a>
-								</div>
+
 							</div>
 						</div>
 					</div>
