7ef4f596ceabffd9d0d93848bce04a8b816484aa
pant411 <pantchanit2561@hotmail.com>
2021-08-04 18:35:25 +0700

    update popup'

--- frontend/app/src/Components/registab5.js
+++ frontend/app/src/Components/registab5.js
@@ -12,22 +12,31 @@ class Registab5 extends React.Component {
 			<div className="Registab5">
 				<div class="regis-box-content">
 					<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
-						<img src="assets/images/bin.png"></img>
+						<img src="assets/images/+.png"></img>
 					</button>
 					<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 						<div class="modal-dialog modal-dialog-centered modal-lg">
 							<div class="modal-content minisize" id="certi_edit">
-								<div class="col-md-5">
-									<input type="text" class="form-control certibtn margin-bottom1" placeholder="พิมพ์ชื่อใบรับรอง/เกียรติบัตร*" required></input>
+								<div class="row" id="input_certi">
+									<div class="col-md-5">
+										<input type="text" class="form-control certibtn margin-bottom1" placeholder="พิมพ์ชื่อใบรับรอง/เกียรติบัตร*" required></input>
+									</div>
+									<div class="col-md-10">
+										<select class="form-select dropbtn_year margin-bottom1 fff" id="yearpicker" required>
+											<option selected disabled value="">ปี*</option>
+										</select>
+									</div>
 								</div>
-								<div class="col-md-5">
-									<select class="form-select dropbtn_year margin-bottom1 fff" id="yearpicker" required>
-										<option selected disabled value="">ปี*</option>
-									</select>
-								</div>
-								<div class="centerverify" id="button_certi">
-									<a type="button" class="btn btn-cta-primary-svshort round profile-button grey margin-right-m" data-bs-dismiss="modal">ยกเลิก</a>
-									<a type="button" class="btn btn-cta-primary-yellowshort profile-button round">เพิ่ม</a>
+								<div class="row" id="input_upload">
+									<div className="bg_upload">
+										<img src="assets/images/upload_file.png" width="70px" height="70px" class="up_img"></img>
+										<h2 className="text_up">อัพโหลดใบรับรองของคุณได้ที่นี่</h2>
+									</div>
+									<div class="col-md-10">
+										<a type="button" class="btn btn-cta-primary-svshort round profile-button grey margin-right-m" data-bs-dismiss="modal">ยกเลิก</a>
+										<a type="button" class="btn btn-cta-primary-yellowshort profile-button round">ลบ</a>
+									</div>
+
 								</div>
 							</div>
 						</div>
