23d258067db59ff66e91738acf43fc991dde7270
pant411 <pantchanit2561@hotmail.com>
2021-08-04 15:26:31 +0700

    update registab5

--- frontend/app/src/Components/registab5.js
+++ frontend/app/src/Components/registab5.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './register.css';
 
 class Registab5 extends React.Component {
+	componentDidMount() {
+		const script = document.createElement("script");
+		script.src = "assets/js/date.js";
+		document.body.appendChild(script);
+	}
 	render() {
 		return (
 			<div className="Registab5">
@@ -12,7 +17,14 @@ class Registab5 extends React.Component {
 					<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 						<div class="modal-dialog modal-dialog-centered modal-lg">
 							<div class="modal-content minisize" id="certi_edit">
-
+								<div class="col-md-5">
+									<input type="text" class="form-control certibtn margin-bottom1" placeholder="พิมพ์ชื่อใบรับรอง/เกียรติบัตร*" required></input>
+								</div>
+								<div class="col-md-5">
+									<select class="form-select dropbtn_year margin-bottom1 fff" id="yearpicker" required>
+										<option selected disabled value="">ปี*</option>
+									</select>
+								</div>
 								<div class="centerverify" id="button_certi">
 									<a type="button" class="btn btn-cta-primary-svshort round profile-button grey margin-right-m" data-bs-dismiss="modal">ยกเลิก</a>
 									<a type="button" class="btn btn-cta-primary-yellowshort profile-button round">เพิ่ม</a>
