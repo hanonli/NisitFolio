d486f7dde04424e08253cca5c64e739e12764ed3
pant411 <pantchanit2561@hotmail.com>
2021-08-04 23:43:33 +0700

    update tabPopup Uncomplete

--- frontend/app/src/register.js
+++ frontend/app/src/register.js
@@ -5,7 +5,7 @@ import Navbarlogo from './Components/navbarlogo';
 import InformationHeader from './Components/informationHeader';
 import reportWebVitals from './reportWebVitals';
 import { Link } from "react-router-dom";
-import Tabs from "./Components/Tabs"; 
+import Tabs from "./Components/Tabs";
 import Registab1 from "./Components/registab1";
 import Registab2 from "./Components/registab2";
 import Registab3 from "./Components/registab3";
@@ -16,30 +16,30 @@ import Registab7 from "./Components/registab7";
 
 class Register extends React.Component {
 
-    componentDidMount() {
+	componentDidMount() {
 		window.addEventListener('load', this.handleLoad);
 		console.log("YEAHXXX!");
 		const script = document.createElement("script");
 		script.src = "assets/js/register.js";
 		document.body.appendChild(script);
-		
+
 	}
-    
-	render (){
+
+	render() {
 		return (
 			<div className="Register">
 				<Navbarlogo />
 				<InformationHeader />
-                <ol class="tabs-list">
-                    <li class="tab-list-item tab-list-active" id="tab-1" type="button">ข้อมูลสำคัญ</li>
-                    <li class="tab-list-item" id="tab-2" type="button">ข้อมูลเพิ่มเติม</li>
-                    <li class="tab-list-item" id="tab-3" type="button">ประวัติการศึกษา</li>
+				<ol class="tabs-list">
+					<li class="tab-list-item tab-list-active" id="tab-1" type="button">ข้อมูลสำคัญ</li>
+					<li class="tab-list-item" id="tab-2" type="button">ข้อมูลเพิ่มเติม</li>
+					<li class="tab-list-item" id="tab-3" type="button">ประวัติการศึกษา</li>
 					<li class="tab-list-item" id="tab-4" type="button">ประวัติการทำงาน</li>
 					<li class="tab-list-item" id="tab-5" type="button">ใบรับรอง</li>
 					<li class="tab-list-item" id="tab-6" type="button">งานที่สนใจ</li>
 					<li class="tab-list-item" id="tab-7" type="button">ทักษะเสริม</li>
-                 </ol>
-				 <div>
+				</ol>
+				<div>
 					<div class="tab-content" id="registab1-content">
 						<Registab1 />
 					</div>
@@ -61,12 +61,12 @@ class Register extends React.Component {
 					<div class="tab-content" id="registab7-content">
 						<Registab7 />
 					</div>
-				 </div>
-                <Link to="/emailverify">
-                    <div class="col block-right">
-                        <a class="btn btn-cta-primary-yellowwide round profile-button" href="#" target="_blank" type="submit">ยืนยันตัวตน</a>
-                    </div>
-                </Link>
+				</div>
+				<Link to="/emailverify">
+					<div class="col block-right">
+						<a class="btn btn-cta-primary-yellowwide round profile-button" href="#" target="_blank" type="submit">ยืนยันตัวตน</a>
+					</div>
+				</Link>
 			</div>
 		);
 	}
