4ddb75bd75a3141ba931c9de4d5adfa30f5c49f5
pant411 <pantchanit2561@hotmail.com>
2021-08-04 11:56:05 +0700

    update register5

--- frontend/app/src/Components/registab5.js
+++ frontend/app/src/Components/registab5.js
@@ -6,7 +6,20 @@ class Registab5 extends React.Component {
 		return (
 			<div className="Registab5">
 				<div class="regis-box-content">
-					<h1>hihi</h1>
+					<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+						<img src="assets/images/bin.png"></img>
+					</button>
+					<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog modal-dialog-centered modal-lg">
+							<div class="modal-content minisize" id="certi_edit">
+
+								<div class="centerverify" id="button_certi">
+									<a type="button" class="btn btn-cta-primary-svshort round profile-button grey margin-right-m" data-bs-dismiss="modal">ยกเลิก</a>
+									<a type="button" class="btn btn-cta-primary-yellowshort profile-button round">เพิ่ม</a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
