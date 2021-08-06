21ea73a157fbaa5bb22f7e480b1d3195a913571b
phattharapolb <phattharapolb@gmail.com>
2021-08-07 00:12:36 +0700

    update study history

--- frontend/app/src/Components/registab3.js
+++ frontend/app/src/Components/registab3.js
@@ -15,13 +15,21 @@ class Registab3 extends React.Component {
 								<img id='icon-plus-circle'  src="assets/images/+.png"></img>
 							</button>
 							<div class="modal fade" id="registabModal1" tabindex="-1" aria-labelledby="exampleModalLabel1" aria-hidden="true">
-                        		<div class="modal-dialog modal-dialog-centered">
-                            		<div class="modal-content minisize">
-                                		<Registab3_addHigher/>
-                                		<div class="centerverify">
-                                        	<a type="button" class="btn btn-cta-primary-svshort round profile-button grey margin-right-m" data-bs-dismiss="modal">ยกเลิก</a>
-                                        	<a type="button" class="btn btn-cta-primary-yellowshort profile-button round">ลบ</a>
-                                		</div>
+                        		<div class="modal-dialog modal-dialog-centered modal-lg">
+                            		<div class="modal-content " >
+										<div class='modal-header'>
+											<h1 class='modal-title' id='regisModallabel1' >เพิ่มประวัติการศึกษา</h1>
+										</div>
+										<div class='modal-body'>
+											<Registab3_addHigher/>
+										</div>
+                                		<div class='modal-footer'>
+				                    		<div class="centerverify">
+												<button type="button" class="btn btn-cta-primary-svshort round profile-button grey margin-right-m" data-bs-dismiss="modal">ยกเลิก</button>
+												<button type="button" class="btn btn-cta-primary-yellowshort profile-button round" data-bs-dismiss='modal'>ตกลง</button>
+                                			</div>
+										</div>
+
                             		</div>
                         		</div>
                     		</div>
@@ -34,7 +42,7 @@ class Registab3 extends React.Component {
 							</button>
 							<div class="modal fade" id="registabModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
                         		<div class="modal-dialog modal-dialog-centered">
-                            		<div class="modal-content maxsize">
+                            		<div class="modal-content largesize">
                                 		<Registab3_addSecondary/>
                                 		<div class="centerverify">
                                         	<a type="button" class="btn btn-cta-primary-svshort round profile-button grey margin-right-m" data-bs-dismiss="modal">ยกเลิก</a>
--- frontend/app/src/Components/registab3_addHigher.js
+++ frontend/app/src/Components/registab3_addHigher.js
@@ -5,23 +5,52 @@ class Registab3_addHigher extends React.Component {
 	render (){
 		return (
 			<div className="Registab3_addHigher">
-				<h1>เพิ่มประวัติการศึกษา</h1>
-                <div class='col-md-4' >
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown button
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#">ปริญญาเอก</a></li>
-                            <li><a class="dropdown-item" href="#">ปริญญาโท</a></li>
-                            <li><a class="dropdown-item" href="#">ปริญญาตรี</a></li>
-                            <li><a class="dropdown-item" href="#">ปริญญาจัตวา</a></li>
-                            <li><a class="dropdown-item" href="#">ปริญญาใจ</a></li>
-                        </ul>
-                    </div>
+				
+                <div class='col-4' >
+                    <div class="selectDropdown">
+                        <select class="form-select form-select-lg dropbtn margin-bottom1 fff" id='selectDropdown1' aria-labelledby="select1">
+                            <option selected disabled value>เลือกวุฒิการศึกษา</option>
+                            <option >ปริญญาเอก</option>    
+                            <option >ปริญญาโท</option>
+                            <option >ปริญญาตรี</option>
+                        </select>
 
+                    </div>
                 </div>
-1
+                <div class='row'>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control dropbtn margin-bottom1" id="school" placeholder="สถานศึกษา*" required></input>
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control dropbtn margin-bottom1" id="statitued" placeholder="คณะ*" required></input>
+                    </div>
+                </div>
+                <div class='row'>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control dropbtn margin-bottom1" id="main_subj" placeholder="สาขาวิชา*" required></input>
+                    </div>
+                    <div class="col-md-6">
+                        <br/>
+                        <h5>เกรดเฉลี่ยรวม</h5>
+                        
+
+                    </div>
+                </div>
+                <div class='col-12' >
+                    <div class="selectDropdown">
+                        <select class="form-select form-select-lg dropbtn margin-bottom1 fff" id='selectDropdown1' aria-labelledby="select1">
+                            <option selected disabled value>เลือกวุฒิการศึกษา</option>
+                            <option >ปริญญาเอก</option>    
+                            <option >ปริญญาโท</option>
+                            <option >ปริญญาตรี</option>
+                        </select>
+                        <br/>
+                    </div>
+                </div>
+
+
+
+
 			
 			</div>
 			
