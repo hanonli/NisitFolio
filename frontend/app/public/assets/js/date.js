5f927abdb5d79f8314601827605c348fa6cec7c2
pant411 <pantchanit2561@hotmail.com>
2021-08-06 15:56:51 +0700

    change file name and add script for upload image

--- frontend/app/public/assets/js/date.js
+++
@@ -1,5 +0,0 @@
-let startYear = 1800;
-let endYear = new Date().getFullYear();
-for (i = endYear; i > startYear; i--) {
-    $('#yearpicker1').append($('<option />').val(i).html(i));
-}
---
+++ frontend/app/public/assets/js/registab5.js
@@ -0,0 +1,13 @@
+/*let startYear = 1800;
+let endYear = new Date().getFullYear();
+for (i = endYear; i > startYear; i--) {
+    $('#yearpicker1').append($('<option />').val(i).html(i));
+}*/
+
+/*----uplode img----*/
+
+$(function () {
+    $('#to_upload112').on('click', function () {
+        $('#image-upload112').click();
+    });
+});
