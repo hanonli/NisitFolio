5ad0f60629faf6d2fee2e0c3ed894c11309a42b6
pant411 <pantchanit2561@hotmail.com>
2021-08-08 21:17:57 +0700

    update script

--- frontend/app/public/assets/js/registab6_popup.js
+++ frontend/app/public/assets/js/registab6_popup.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
     $('.tabs_pop li').on('click', function () {
-        var tabId = $(this).attr('data-tab1')
-        $('.tabs_pop li').removeClass('current2')
-        $('.tab-pane_pop').removeClass('current2')
-        $(this).addClass('current2')
-        $('#' + tabId).addClass('current2')
+        var tabId = $(this).attr('data-tab1');
+        $('.tabs_pop li').removeClass('current2');
+        $('.tab-pane_pop').removeClass('current2');
+        $(this).addClass('current2');
+        $('#' + tabId).addClass('current2');
     })
 });
