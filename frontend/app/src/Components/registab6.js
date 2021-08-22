3d5f5c9d1208f580dc75e98a63ece648ee7d7e0f
pant411 <pantchanit2561@hotmail.com>
2021-08-22 17:08:33 +0700

    add slider range dirty tick marks

--- frontend/app/src/Components/registab6.js
+++ frontend/app/src/Components/registab6.js
@@ -96,7 +96,7 @@ class Registab6 extends React.Component {
 												</div>
 												<div class="col-md-5">
 													<div className="range-slider-job">
-														<input type="range" class="form-control-range" min="0" max="10" defaultValue="5" id="customRange11" />
+														<input type="range" class="slider x1" min="0" max="100" step="1" defaultValue="50" id="customRange11"/>
 													</div>
 												</div>
 											</div>
@@ -113,7 +113,7 @@ class Registab6 extends React.Component {
 												</div>
 												<div class="col-md-5">
 													<div className="range-slider-job">
-														<input type="range" class="form-control-range" min="0" max="10" defaultValue="5" id="customRange11" />
+														<input type="range" class="slider x2" min="0" max="100" step="1" defaultValue="50" id="customRange12"/>
 													</div>
 												</div>
 											</div>
@@ -130,12 +130,12 @@ class Registab6 extends React.Component {
 												</div>
 												<div class="col-md-5">
 													<div className="range-slider-job">
-														<input type="range" class="slider" min="0" max="10" step="0.1" defaultValue="5" id="customRange11" list='tickmarks1'/>
-														<div id="tickmarks1">
-															<p>ไม่ได้</p>
-															<p>พอได้เล็กน้อย</p>
-															<p>ดี</p>
-															<p>ยอดเยี่ยม</p>
+														<input type="range" class="slider x3" min="0" max="100" step="1" defaultValue="50" id="customRange13"/>
+														<div className="mark3">
+															<label>ไม่ได้</label>
+															<label>พอได้เล็กน้อย</label>
+															<label>ดี</label>
+															<label>ยอดเยี่ยม</label>
 														</div>
 													</div>
 												</div>
