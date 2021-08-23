1fb378d9e0c33f7bd940eaa7593c3fb092d1133d
pant411 <pantchanit2561@hotmail.com>
2021-08-23 09:22:12 +0700

    Merge branch 'main' of https://github.com/hanonli/NisitFolio into main

--- frontend/app/public/assets/js/registab6_popup.js
+++ frontend/app/public/assets/js/registab6_popup.js
@@ -10,27 +10,23 @@ $(document).ready(function () {
 
 });
 
-var value1,value2,value3;
+/*---control slider range---*/
+var value1, value2, value3;
 
-$(document).ready(function () {
+document.getElementById("customRange11").oninput = function () {
+    value1 = (this.value - this.min) / (this.max - this.min) * 100;
+    this.style.background = 'linear-gradient(to right, #f0a143 0%, #f0a143 ' + value1 + '%, #c4c4c4 ' + value1 + '%, #c4c4c4 100%)';
+    console.log(`skill1: ` + value1);
+};
 
-    document.getElementById("customRange11").oninput = function() {
-        value1 = (this.value-this.min)/(this.max-this.min)*100;
-        this.style.background = 'linear-gradient(to right, #f0a143 0%, #f0a143 ' + value1 + '%, #c4c4c4 ' + value1 + '%, #c4c4c4 100%)';
-        console.log(value1);
-    };
+document.getElementById("customRange12").oninput = function () {
+    value2 = (this.value - this.min) / (this.max - this.min) * 100;
+    this.style.background = 'linear-gradient(to right, #0fe17c 0%, #0fe17c ' + value2 + '%, #c4c4c4 ' + value2 + '%, #c4c4c4 100%)';
+    console.log(`skill2: ` + value2);
+};
 
-    document.getElementById("customRange12").oninput = function() {
-        value2 = (this.value-this.min)/(this.max-this.min)*100;
-        this.style.background = 'linear-gradient(to right, #0fe17c 0%, #0fe17c ' + value2 + '%, #c4c4c4 ' + value2 + '%, #c4c4c4 100%)';
-        console.log(value2);
-    };    
-
-    document.getElementById("customRange13").oninput = function() {
-        value3 = (this.value-this.min)/(this.max-this.min)*100;
-        this.style.background = 'linear-gradient(to right, #c98a11 0%, #c98a11 ' + value3 + '%, #c4c4c4 ' + value3 + '%, #c4c4c4 100%)';
-        console.log(value3);
-    };    
-
-});
-
+document.getElementById("customRange13").oninput = function () {
+    value3 = (this.value - this.min) / (this.max - this.min) * 100;
+    this.style.background = 'linear-gradient(to right, #c98a11 0%, #c98a11 ' + value3 + '%, #c4c4c4 ' + value3 + '%, #c4c4c4 100%)';
+    console.log(`skill3: ` + value3);
+};
