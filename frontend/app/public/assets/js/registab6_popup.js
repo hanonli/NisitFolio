f598e689686081f405e9e494fa27a72d583eae1c
pant411 <pantchanit2561@hotmail.com>
2021-08-26 11:43:31 +0700

    update

--- frontend/app/public/assets/js/registab6_popup.js
+++ frontend/app/public/assets/js/registab6_popup.js
@@ -10,33 +10,25 @@ $(document).ready(function () {
 
 });
 
-var value1,value2,value3;
+/*----control slider range----*/
 
 $(document).ready(function () {
-
-    document.getElementById("customRange11").oninput = function() {
-        value1 = (this.value-this.min)/(this.max-this.min)*100;
+    var value1, value2, value3;
+    document.getElementById("customRange11").oninput = function () {
+        value1 = (this.value - this.min) / (this.max - this.min) * 100;
         this.style.background = 'linear-gradient(to right, #f0a143 0%, #f0a143 ' + value1 + '%, #c4c4c4 ' + value1 + '%, #c4c4c4 100%)';
-<<<<<<< HEAD
-        console.log(value1);
-=======
         console.log(`skill1: ${value1 / 10}`);
->>>>>>> cc62a96d4c1d2ba5aa7be0f0560291ce4085979b
     };
 
-    document.getElementById("customRange12").oninput = function() {
-        value2 = (this.value-this.min)/(this.max-this.min)*100;
+    document.getElementById("customRange12").oninput = function () {
+        value2 = (this.value - this.min) / (this.max - this.min) * 100;
         this.style.background = 'linear-gradient(to right, #0fe17c 0%, #0fe17c ' + value2 + '%, #c4c4c4 ' + value2 + '%, #c4c4c4 100%)';
-        console.log(value2);
-    };    
+        console.log(`skill2: ` + value2 / 10);
+    };
 
-    document.getElementById("customRange13").oninput = function() {
-        value3 = (this.value-this.min)/(this.max-this.min)*100;
+    document.getElementById("customRange13").oninput = function () {
+        value3 = (this.value - this.min) / (this.max - this.min) * 100;
         this.style.background = 'linear-gradient(to right, #c98a11 0%, #c98a11 ' + value3 + '%, #c4c4c4 ' + value3 + '%, #c4c4c4 100%)';
-<<<<<<< HEAD
-        console.log(value3);
-    };    
-=======
         console.log(`skill3: ` + value3 / 10);
     };
 });
@@ -68,8 +60,8 @@ $(document).ready(function () {
 
 var name_job, skill_job_1, score_skill_job_1, skill_job_2, score_skill_job_2, skill_job_3, score_skill_job_3, obj_job_1, obj_job_2, obj_job_3;
 
-    /*----submit for add job----*/
-$(document).ready(function () { 
+/*----submit for add job----*/
+$(document).ready(function () {
     $("#add-job").click(function () {
         console.log('Oh nha hee1');
         $("#exampleModalJob")[0].reset();
@@ -122,7 +114,10 @@ $(document).ready(function () {
         console.log(`obj_job_2: `, obj_job_2);
         console.log(`obj_job_3: `, obj_job_3);
     });
->>>>>>> cc62a96d4c1d2ba5aa7be0f0560291ce4085979b
 
 });
 
+/*----remove job----*/
+$('#del-job').click(function () {
+    $("#no-2").remove();
+});
