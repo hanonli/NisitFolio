02069309071a054c5eeac33231f1d91aae096613
pant411 <pantchanit2561@hotmail.com>
2021-08-27 14:00:21 +0700

    update preview img

--- frontend/app/public/assets/js/registab5.js
+++ frontend/app/public/assets/js/registab5.js
@@ -17,6 +17,9 @@ $(document).ready(function () {
 
 //gu loke ma jak stackoverflow because gu stupid jquery
 
+//preview img before upload
+
+
 /*-----value of modal-----*/
 
 $(document).ready(function () {
@@ -24,7 +27,18 @@ $(document).ready(function () {
     console.log(modal_tab5);
 });
 
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#preview_before_upload').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
 $(document).ready(function () {
+
     var choose_function = -1; //default
     $("#edit-certi").click(function () {
         choose_function = 1;
@@ -39,6 +53,12 @@ $(document).ready(function () {
         $('#exampleModal11112').modal('show');
         document.querySelector('#submit-certi').innerText = 'เพิ่ม';
     });
+    document.getElementById("image-upload112").addEventListener("change", function () {
+        $("#icon-upload-112").remove();
+        $("#text-upload-112").remove();
+        readURL(document.getElementById("image-upload112"));
+        $(".for_upload112").append('<img id="preview_before_upload" height="120"></img>');
+    });
     document.getElementById("submit-certi").addEventListener("click", function () {
         if (choose_function == 2) {
             console.log("add!!!!!!");
@@ -51,6 +71,9 @@ $(document).ready(function () {
         }
         else if (choose_function == 1) {
             console.log("edit!!!!!!");
+            $("#icon-upload-112").remove();
+            $("#text-upload-112").remove();
+            $(".for_upload112").append('<img id="preview_before_upload" height="120"></img>'); //unconplete
             name_certi = document.getElementById("nm_certi").value;
             year_certi = document.getElementById("yearpicker_111").value;
             file_pic_certi = document.getElementById("image-upload112");
@@ -58,8 +81,10 @@ $(document).ready(function () {
             console.log(`year: `, year_certi);
             console.log('pic: ', file_pic_certi.files[0]);
         }
-
         $("#nm_certi").val("");
         $("#yearpicker_111").prop('selectedIndex', 0);
+        $("#preview_before_upload").remove();
+        $(".for_upload112").append('<img id="icon-upload-112" src="assets/images/upload_file.png" width="85px" height="85px" class="up_img"></img>');
+        $(".for_upload112").append('<h2 className="text_up" id="text-upload-112">อัพโหลดใบรับรองของคุณได้ที่นี่</h2>');
     });
 });
--- frontend/app/src/Components/registab5.js
+++ frontend/app/src/Components/registab5.js
@@ -83,9 +83,9 @@ class Registab5 extends React.Component {
 								<div class="row" id="input_upload">
 									<input id="image-upload112" accept="images/pic_Of_Certi/*" class="hidden" type="file"></input>
 									<div className="bg_upload" id="to_upload112">
-										<div className="for_upload112">
-											<img src="assets/images/upload_file.png" width="85px" height="85px" class="up_img"></img>
-											<h2 className="text_up">อัพโหลดใบรับรองของคุณได้ที่นี่</h2>
+										<div className="for_upload112" id="to_upload116">
+											<img id="icon-upload-112" src="assets/images/upload_file.png" width="85px" height="85px" class="up_img"></img>
+											<h2 className="text_up" id="text-upload-112">อัพโหลดใบรับรองของคุณได้ที่นี่</h2>
 										</div>
 									</div>
 
