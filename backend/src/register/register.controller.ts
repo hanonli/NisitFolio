11e38fd4152276087ddf5df55e4b66b5350ffaad
Phutthawan <phutthawan.h@ku.th>
2021-09-09 21:43:10 +0700

    update updateprofile

--- backend/src/register/register.controller.ts
+++ backend/src/register/register.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get, Post, Body, Param, Request, Patch, UseGuards } from '@
 import { RegisterService } from './register.service';
 import { CreateRegisDto } from './dto/create-register.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { EditProfileDto2 } from './dto/editprofile2.dto';
 
 
 @Controller('register')
@@ -43,6 +44,13 @@ export class RegisterController {
   async GetInfo(@Request() req)
   {
     return this.registerService.GetInfo(req.user.userId);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Patch()
+    async UpdateProfile(@Body() CreateDto: EditProfileDto2,@Request() req) {
+      const x=req.user.userId
+      //const x="613a13762a58701a949f5d19";
+    return this.registerService.UpdatProfile(x,CreateDto);
   }
 
 }
