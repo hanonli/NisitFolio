6ca28a13cd7260bf453680ea7bec3c0de4043e40
rungrung64 <rungrung64@hotmail.com>
2021-09-11 19:18:22 +0700

    t

--- frontend/app/src/Components/sharingpopup.js
+++ frontend/app/src/Components/sharingpopup.js
@@ -5,21 +5,30 @@ class SharingPopup extends React.Component {
 
     return (
         <div>
-        <div class="modal fade" id="sharingResume" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+        <div class="modal fade" id="sharingResume" aria-hidden="true" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content container-fluid sharingSize">
                     <div class="row">
                         <p class="SharingFontHead col-10">Sharing</p>
                     </div>
-                    <div class="row">
-                        <img class="col-4 transition-component scale-up-s" src="assets/images/share_link.png" id="cross-fade" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharinglink"/>
-                        <img class="col-4 transition-component scale-up-s" src="assets/images/share_qr.png" id="cross-fade" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharingqr"/>
-                        <img class="col-4 transition-component scale-up-s" src="assets/images/share_pdf.png" id="cross-fade" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharingpdf"/>
+                    <div class="row mg-l1">
+                        <div class="col-4 transition-component scale-up-s" id="cross-fade">
+                            <img class="col-10 bottom" src="assets/images/share_link_hover.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharinglink"/>
+                            <img class="col-10  top" src="assets/images/share_link.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharinglink"/>
+                        </div>
+                        <div class="col-4 transition-component scale-up-s" id="cross-fade">
+                            <img class="col-10 bottom" src="assets/images/share_qr_hover.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharingqr"/>
+                            <img class="col-10 top" src="assets/images/share_qr.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharingqr"/>
+                        </div>
+                        <div class="col-4 transition-component scale-up-s" id="cross-fade">
+                        <img class="col-10 bottom" src="assets/images/share_pdf_hover.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharingpdf"/>
+                        <img class="col-10 top" src="assets/images/share_pdf.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharingpdf"/>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="modal fade" id="sharinglink" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+        <div class="modal fade" id="sharinglink" aria-hidden="true" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content sharingSize container-fluid">
                     <div class="row">
