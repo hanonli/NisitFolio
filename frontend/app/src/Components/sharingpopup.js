df7c9a718510370c49ba0a1bbbcce509975b8983
rungrung64 <rungrung64@hotmail.com>
2021-09-11 17:50:30 +0700

    t

--- frontend/app/src/Components/sharingpopup.js
+++ frontend/app/src/Components/sharingpopup.js
@@ -6,13 +6,12 @@ class SharingPopup extends React.Component {
         <div>
         <div class="modal fade" id="sharingResume" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content sharingSize">
-                    <div class="modal-header container-fluid">
-                        <h1 class="modal-title SharingFontHead">Sharing</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-content sharingSize container-fluid">
+                    <div class="row">
+                        <h1 class="SharingFontHead col-10">Sharing</h1>
                     </div>
                     <div class="modal-body row">
-                        Show a second modal and hide this one with the button below.
+                        <img class="obj-icon tooltips-item col-4" src="assets/images/sharingIcon.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharinglink"/>
                         <button class="btn btn-primary col-2" data-bs-target="#sharinglink" data-bs-toggle="modal" data-bs-dismiss="modal">Link</button>
                     </div>
                 </div>
@@ -20,15 +19,10 @@ class SharingPopup extends React.Component {
         </div>
         <div class="modal fade" id="sharinglink" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title SharingFontHead" id="exampleModalToggleLabel2">Sharing link</h1>
+                <div class="modal-content sharingSize">
+                    <div class="row">
+                        <h1 class="SharingFontHead" id="exampleModalToggleLabel2">Sharing link</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Hide this modal and show the first with the button below.
-                    </div>
-                    <div class="modal-footer">
                         <button class="btn btn-primary" data-bs-target="#sharingresume" data-bs-toggle="modal" data-bs-dismiss="modal">Back to Sharing</button>
                     </div>
                 </div>
