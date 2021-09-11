437b722b432390f01be8cb7f1f85bb3b76adc7ae
rungrung64 <rungrung64@hotmail.com>
2021-09-11 18:46:11 +0700

    t

--- frontend/app/src/Components/sharingpopup.js
+++ frontend/app/src/Components/sharingpopup.js
@@ -6,24 +6,44 @@ class SharingPopup extends React.Component {
         <div>
         <div class="modal fade" id="sharingResume" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content sharingSize container-fluid">
+                <div class="modal-content container-fluid sharingSize">
                     <div class="row">
-                        <h1 class="SharingFontHead col-10">Sharing</h1>
+                        <p class="SharingFontHead col-10">Sharing</p>
                     </div>
-                    <div class="modal-body row">
-                        <img class="obj-icon tooltips-item col-4" src="assets/images/sharingIcon.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharinglink"/>
-                        <button class="btn btn-primary col-2" data-bs-target="#sharinglink" data-bs-toggle="modal" data-bs-dismiss="modal">Link</button>
+                    <div class="row">
+                        <img class="col-4" src="assets/images/share_link.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharinglink"/>
+                        <img class="col-4" src="assets/images/share_qr.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharingqr"/>
+                        <img class="col-4" src="assets/images/share_pdf.png" type="button" data-bs-toggle="modal" toggle-type="dynamic" data-bs-target="#sharingpdf"/>
                     </div>
                 </div>
             </div>
         </div>
         <div class="modal fade" id="sharinglink" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content sharingSize">
+                <div class="modal-content sharingSize container-fluid">
                     <div class="row">
-                        <h1 class="SharingFontHead" id="exampleModalToggleLabel2">Sharing link</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        <button class="btn btn-primary" data-bs-target="#sharingresume" data-bs-toggle="modal" data-bs-dismiss="modal">Back to Sharing</button>
+                        <h1 class="SharingFontHead col-10" id="exampleModalToggleLabel2">Sharing</h1>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="sharingpdf" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content sharingSize container-fluid">
+                    <div class="row">
+                        <h1 class="SharingFontHead col-10" id="exampleModalToggleLabel2">Sharing PDF</h1>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="sharingqr" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content sharingSize container-fluid">
+                    <div class="row">
+                        <h1 class="SharingFontHead col-10" id="exampleModalToggleLabel2">Sharing QR</h1>
+
                     </div>
                 </div>
             </div>
