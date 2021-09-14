1d2120bcd37b285af03b62fa0da72c5305421739
rungrung64 <rungrung64@hotmail.com>
2021-09-14 16:09:37 +0700

    fix bugs

--- frontend/app/src/Components/myresumeContent.js
+++ frontend/app/src/Components/myresumeContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cookie from 'react-cookies';
 import SharingPopup from './sharingpopup';
 import MyResume1 from './myresume1';
 import MyResume2 from './myresume2';
@@ -7,6 +8,45 @@ import MyResumeEx from './myResumeEx';
 import './myresume.css';
 
 class MyResumeContent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleLoad = this.handleLoad.bind(this);
+	 }
+	
+	componentDidMount() {
+		/*Func GET UserID To Get Resume from UserId and then find ResumeId to show*/
+		var token = cookie.load('login-token')
+		fetch("http://localhost:2000/profile/",{
+			method: "GET",
+			headers: {
+				'Authorization': 'Bearer '+token,
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "*",
+				"Access-Control-Allow-Credentials": true,
+				"Content-Type": "application/json"
+			},
+		})
+		.then(function(response) {
+			return response.text().then(function(text) {
+              console.log("UserID : "+ text);
+			});
+		 });
+		 
+		window.addEventListener('load', this.handleLoad);
+		console.log("YEAHXXX!");
+		const script = document.createElement("script");
+		script.src = "assets/js/myresume.js";
+		document.body.appendChild(script);
+		
+	}
+	
+	componentWillUnmount() { 
+	   window.removeEventListener('load', this.handleLoad)  
+	}
+	
+	handleLoad() {
+		console.log("YEAH!");
+	 }
 	render (){
 		return (
 			<div className="MyResumeContent">
