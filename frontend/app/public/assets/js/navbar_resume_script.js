8ce6475061ddd7c8aaca08fbd1196006aecde670
phattharapolb <phattharapolb@gmail.com>
2021-09-15 21:58:58 +0700

    update

--- frontend/app/public/assets/js/navbar_resume_script.js
+++ frontend/app/public/assets/js/navbar_resume_script.js
@@ -1,6 +1,11 @@
 console.log('using resume script')
-$( ".MyResumeContent" ).ready(function() {
-    $('.MyResumeContent').css("padding-left",'50px');
+$(".Home" ).ready(function() {
+    $('.Home').css('overflow','hidden');
+
+});
+
+$(".MyResumeContent" ).ready(function() {
+    $('.MyResumeContent').css("padding-left",'40px');
 });
 
 
@@ -10,14 +15,20 @@ $( "#slidenav" ).click(function() {
     if(!toggle){
         // alert('toggle open')
         $("#sideNav").css("width", "50%");
+        $('.MyResumeContent').css("width","80%")
         $("#icon-slide_right").css("transform", "rotate(180deg)");
-        $(".sidenav-centered").css("left", "98%");
+        $(".sidenav-centered").css("left", "97%");
+        $('.MyResumeContent').css("padding-left","50%");
+        $('.MyResumeContent').css("padding-left","50%");
+
+
     }
     else if(toggle){
         // alert('toggle close')
         $("#sideNav").css("width", "40px");
         $("#icon-slide_right").css("transform", "rotate(0deg)");
         $(".sidenav-centered").css("left", "50%");
+        $('.MyResumeContent').css("padding-left","40px");
     }
     toggle=!toggle;  
 });
