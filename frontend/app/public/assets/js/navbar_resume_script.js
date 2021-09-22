5996092542b24b3e1d918f3799036014c1534c3d
phattharapolb <phattharapolb@gmail.com>
2021-09-22 16:20:14 +0700

    add responsive

--- frontend/app/public/assets/js/navbar_resume_script.js
+++ frontend/app/public/assets/js/navbar_resume_script.js
@@ -2,9 +2,11 @@ $(document).ready( function() {
     console.log('using resume script')
     $('#icon-myresume-education, #icon-myresume-certi, #icon-myresume-skill, #icon-myresume-work, #icon-myresume-goal, #icon-myresume' )
     .attr({
-        'margin' : '100px 100px 100px 100px',
-        'width': ' 45px',
-        'height' : '45px',
+        
+        'width': ' 40%',
+        'height' : '40%',
+        "padding-top" : "0vh",
+        "margin-bottom" : "0vh"
     });
     $(".resumeblock").hover(function(){
         $(this).css("background-color", "#e6ba4e");
@@ -20,55 +22,41 @@ $(document).ready( function() {
     $('#icon-myresume-goal').attr('title', 'Goal');
     
 
-    
-      
 });
 
 
 
 $('.Home').css('overflow','hidden');
-$('.MyResumeContent').css("padding-left",'6%');
+$('.MyResumeContent').css("padding-left",'6vw');
+$('.Resume_sideNavbar').css("transition","0.3s");
+$('.resumeblock').css({
+    'width' : '6vw',
+    'height' : '16.67vh',
+    "padding-top" : "3.03vw",
 
+});
 
-$("#icon-myresume").attr('title', 'Bookmark');
-$("#icon-myresume").attr('title', 'Education');
 
 $(window).on("scroll load resize", function(){
+    
     if($(window).scrollTop() > 0){
-        $('.Resume_sideNavbar').css("padding-top","2%");
-        $('.Resume_sideNavbar').css("transition","0.3s");
+        $('.Resume_sideNavbar').css("padding-top","0vh");
+        $('.resumeblock').css({
+            "height": "8.47vw",
+            "padding-top" : "3.03vw",
+        });
+
     }else{
-        $('.Resume_sideNavbar').css("padding-top","4%");
-        $('.Resume_sideNavbar').css("padding-bottom","10%");
-        $('.Resume_sideNavbar').css("transition","0.3s");
+        $('.Resume_sideNavbar').css({
+            "padding-top":"3.4vw",
+            "padding-bottom":"0vh"
+        });
+        $('.resumeblock').css({
+            "height": "7.9vw",
+            "padding-top" : "2.8vw",
+        });
+
     }
 });
 
 
-
-
-// let toggle=false;
-// $( "#slidenav" ).click(function() {
-//     // alert(`toggle ${toggle}`)
-//     if(!toggle){
-//         // alert('toggle open')
-//         $("#sideNav").css("width", "50%");
-//         $('.MyResumeContent').css("width","80%")
-//         $("#icon-slide_right").css("transform", "rotate(180deg)");
-//         $(".sidenav-centered").css("left", "97%");
-//         $('.MyResumeContent').css("padding-left","50%");
-//         $('.MyResumeContent').css("padding-left","50%");
-
-
-//     }
-//     else if(toggle){
-//         // alert('toggle close')
-//         $("#sideNav").css("width", "100px");
-//         $("#icon-slide_right").css("transform", "rotate(0deg)");
-//         $(".sidenav-centered").css("left", "80%");
-//         $('.MyResumeContent').css("padding-left","100px");
-//     }
-//     toggle=!toggle;  
-// });
-
-  
