8ea1d469a77e685a6872d59f78775ac19d381946
phattharapolb <phattharapolb@gmail.com>
2021-09-23 19:34:32 +0700

    topnav

---
+++ frontend/app/src/Components/navbar_top_resume.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+class Resume_topNavbar extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.handleLoad = this.handleLoad.bind(this);
+	 }
+	
+	componentDidMount() {
+
+		const script = document.createElement("script");
+		script.src = "assets/js/navbar_top_resume_script.js";
+		document.body.appendChild(script);
+		
+	}
+
+	
+	render (){
+
+		return (
+			
+			<div className="Resume_topNavbar" id='topNav'>
+				
+				<div  id='myresumetoppath'> 
+						
+				</div>
+				{/* <div className='sidenav-centered'>
+					<button type="button" class="slide_button" id='slidenav'>
+						<img id='icon-slide_right'  src="assets/images/slide_right.png"></img>
+					</button>
+					
+				</div> */}
+				
+			</div>
+			
+		);
+	}
+}
+
+export default Resume_topNavbar;
