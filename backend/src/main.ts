91c2972c28ddb38adcb1fab869ad9fd2978016e0
SirapopPCC <sirapopjpt@gmail.com>
2021-10-01 15:20:47 +0700

    app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,14 @@
 import { APP_PIPE, NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   app.enableCors();
   await app.listen(2000);
 }
