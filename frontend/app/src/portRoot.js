760c97155d84f31387a116efcc647e12d210cc43
lazycodex <Hope_Of_dfinal@hotmail.com>
2021-10-11 01:55:41 +0700

    update portfolio layout

--- frontend/app/src/portRoot.js
+++ frontend/app/src/portRoot.js
@@ -115,6 +115,11 @@ class PortRoot extends React.Component {
 						index += 1;	
 					});
 					
+					$('#new-port').on('click', function(){
+						  cookie.save('port-entry', 'new', { path: '/' })
+						  refThis.setState({ redirect: "/editport" });
+					  });
+					
 					$('.pft-edit-icon').on('click', function(e){
 							e.stopPropagation();
 							cookie.save('port-entry', 'edit', { path: '/' })
@@ -225,12 +230,12 @@ class PortRoot extends React.Component {
 					// .scrollTop() retrieves vertical position of the scroll bar for the first element in a set of matched elements
 					var scroll = $(window).scrollTop();
 
-					var objectSelect = $('#anim-pos');
+					//var objectSelect = $('#anim-pos');
 					
 					// .offset() retrieves current position of element relative to document
-					var objectPosition = objectSelect.offset().top;
+					//var objectPosition = objectSelect.offset().top;
 
-					if (scroll > objectPosition+100) {
+					if (scroll > 0) {
 						console.log("PostAnim");
 						$('.pt-header').addClass('pt-header-anim');
 						$('.pt-name').addClass('pt-name-anim');
@@ -245,11 +250,11 @@ class PortRoot extends React.Component {
 					}
 				  });
 				  
-				  $(".dynamic-footer-arrow").click(function() {
+				  /*$(".dynamic-footer-arrow").click(function() {
 						$('html, body').animate({
 							scrollTop: $("#anim-pos").offset().top+120
 						}, 1);
-					});
+					});*/
 				}
 
 				
@@ -337,23 +342,10 @@ class PortRoot extends React.Component {
 								</div>
 							</div>        
 						</div>
-						<a class="btn btn-cta-primary-yellow round profile-button pf-fx" target="_blank">เพิ่มกิจกรรมของคุณ</a>
+						<a class="btn btn-cta-primary-yellow round profile-button pf-fx" id="new-port" target="_blank">เพิ่มกิจกรรมของคุณ</a>
 					</header>
 					<div class="pt-pad"></div>
-					<div class="nf-flex">
-						<header class="header header-azure hf-header round" id="empty-zone">
-							<img class="hf-i" src="assets/images/outline_add_black_24dp hd.png" alt=""/>
-							<abf class="hf-n" id="anim-pos">เพิ่มผลงานเจ๋ง ๆ ของคุณได้ที่นี่</abf>
-						</header>
-					</div>
-					<br></br><br></br>
 					<div class="port-box">
-						<br></br>
-						<div class="dfa-flex">
-							<img class="dynamic-footer-arrow" src="assets/images/arrow_up1.png"></img>
-							<div class="dynamic-footer-label">ผลงานของคุณ</div>
-						</div>
-						<br></br>
 						<div class="pft-flex" id="pft-box">
 
 						</div>
