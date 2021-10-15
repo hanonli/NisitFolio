6b17515b9b0bbdc8d627a76022ed19e5287c0a8c
Phutthawan <phutthawan.h@ku.th>
2021-10-15 17:30:44 +0700

    error  git หัว..

--- backend/portfolio/dto/delete-port.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEmail, IsNotEmpty, IsNumberString } from 'class-validator';
-import { ObjectId } from 'mongodb';
-export class DeletePortfolioDto {
-
-    UserId: ObjectId;
-
-    PortId: ObjectId;
-}
