df9489c27ce2d67fcd17e6e3adc399f3592ac25a
Phutthawan <phutthawan.h@ku.th>
2021-10-18 08:03:54 +0700

    push ผิด

--- backend/src/myresume/myresume.service.ts
+++ backend/src/myresume/myresume.service.ts
@@ -465,7 +465,7 @@ export class MyResumeService {
     //return [userid,UserId]this.resumeModel.find({UserId : userId});
     
     const resumeColor=(await this.resumeModel.findOne({UserId:UserId}));
-    result.Color_ResumeId=resumeColor;
+    result.Color_ResumeId=resumeColor.Color;
     
     
 
